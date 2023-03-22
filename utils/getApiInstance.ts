7c1f1461b9dad953d21dfba3c15a358863c8bf4e
GXU3KG <72329194+GXU3KG@users.noreply.github.com>
2023-03-22 21:41:07 +0800

    original

--- utils/getApiInstance.ts
+++ utils/getApiInstance.ts
@@ -23,8 +23,11 @@ export function getAPIInstance(OPENAI_API_KEY: string): ChatGPTAPI {
 }
 
 function getOpenaiApiKeyAliasMap() {
-  const envValue = "hzh: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|twx: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";
-  
+  const envValue = env.OPENAI_API_KEY_ALIAS;
+  if (!envValue) {
+    return {};
+  }
+
   let openaiApiKeyAliasMap: Record<string, string> = {};
 
   const aliasList = envValue.split('|');
